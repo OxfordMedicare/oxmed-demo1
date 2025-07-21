e500cc7c7b86ad65690dd890fcea4bad26c7c901
Eric O <eric@Priscillas-MacBook-Air.local>
2025-07-21 09:31:48 +0100

    Deploy: updated Flutter web build to Firebase

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c387c3b36d525047af4938b3ea6757f8",
+const RESOURCES = {"flutter_bootstrap.js": "e494774fd929a261a57c4790abbaddf5",
 "version.json": "8a4068bf154c6a7521fbc3739cc66513",
 "index.html": "2f434fa0d41d31589f96974e23584990",
 "/": "2f434fa0d41d31589f96974e23584990",
